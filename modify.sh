d301a0d08a37524361d8b9a8539c3ae9db87d70b
BxOxSxS <blazejsadowski2004@gmail.com>
2020-01-25 22:38:08 +0100

    Remove umount from modify Because of this snap can still work during modify

--- modify.sh
+++ modify.sh
@@ -4,7 +4,6 @@ mount=/var/lib/snapd/snap/
 if [ "$1" == "modify" ];  then
      read -p "Enter snap name e.g. spotify "  name
      read -p "Enter snap revision e.g. 38 " rev
-     sudo umount ${mount}${name}/$rev
 
      sudo unsquashfs /var/lib/snapd/snaps/${name}_${rev}.snap
      sudo chmod -R 777  squashfs-root
@@ -16,6 +15,7 @@ elif [ "$1" == "done" ]; then
      read -p "Enter snap name e.g. spotify "  name
      read -p "Enter snap rev e.g. 38 " rev
 
+     sudo umount ${mount}${name}/$rev
      sudo mksquashfs ${name}_${rev} ${name}_${rev}.snap
      sudo cp ${name}_${rev}.snap /var/lib/snapd/snaps/${name}_${rev}.snap
      sudo mount -t squashfs -o ro,nodev,relatime,x-gdu.hide /var/lib/snapd/snaps/${name}_${rev}.snap ${mount}${name}/$rev

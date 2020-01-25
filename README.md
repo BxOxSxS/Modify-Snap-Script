1111111d7930817e5a75eca13a976e954be13ba4
BxOxSxS <BxOxSxS@protonmail.com>
2020-01-25 13:23:49 +0100

    Variable clean up

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Modify Snap Script
 This is small script that unmount snap and unsquashfs it to let you modify it. After that you can mount it with changes.
 
-**Note: the default snap mount folder is** `/var/lib/snapd/snap/` **if your distribution has a different one, change the**  `snapM` **value in the script**
+**Note: the default snap mount folder is** `/var/lib/snapd/snap/` **if your distribution has a different one, change the**  `mount` **variable in the script**
 ## How to use
 After starting script it will ask you about snap name and revision.
 * Use `modify` to unmount snap. Snap will be in folder `[snap name]_[revision number]`
--- modify.sh
+++ modify.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-snapM=/var/lib/snapd/snap/
+mount=/var/lib/snapd/snap/
 
 if [ "$1" == "modify" ];  then
-     read -p "Enter snap name e.g. spotify "  snapN
-     read -p "Enter snap revision e.g. 38 " snapR
-     sudo umount ${snapM}${snapN}/$snapR
+     read -p "Enter snap name e.g. spotify "  name
+     read -p "Enter snap revision e.g. 38 " rev
+     sudo umount ${mount}${name}/$rev
 
-     sudo unsquashfs /var/lib/snapd/snaps/${snapN}_${snapR}.snap
+     sudo unsquashfs /var/lib/snapd/snaps/${name}_${rev}.snap
      sudo chmod -R 777  squashfs-root
-     mv squashfs-root ${snapN}_${snapR}
-     echo Now you can modify snap in folder ${snapN}_${snapR}
+     mv squashfs-root ${name}_${rev}
+     echo Now you can modify snap in folder ${name}_${rev}
      echo Call me with done parametr to mount snap
 
 elif [ "$1" == "done" ]; then
-     read -p "Enter snap name e.g. spotify "  snapN
-     read -p "Enter snap Rev e.g. 38 " snapR
+     read -p "Enter snap name e.g. spotify "  name
+     read -p "Enter snap rev e.g. 38 " rev
 
-     sudo mksquashfs ${snapN}_${snapR} ${snapN}_${snapR}.snap
-     sudo cp ${snapN}_${snapR}.snap /var/lib/snapd/snaps/${snapN}_${snapR}.snap
-     sudo mount -t squashfs -o ro,nodev,relatime,x-gdu.hide /var/lib/snapd/snaps/${snapN}_${snapR}.snap ${snapM}${snapN}/$snapR
-     sudo rm -r ${snapN}_${snapR}
-     sudo rm ${snapN}_${snapR}.snap
+     sudo mksquashfs ${name}_${rev} ${name}_${rev}.snap
+     sudo cp ${name}_${rev}.snap /var/lib/snapd/snaps/${name}_${rev}.snap
+     sudo mount -t squashfs -o ro,nodev,relatime,x-gdu.hide /var/lib/snapd/snaps/${name}_${rev}.snap ${mount}${name}/$rev
+     sudo rm -r ${name}_${rev}
+     sudo rm ${name}_${rev}.snap
 
 else
      echo Unkown parametr
